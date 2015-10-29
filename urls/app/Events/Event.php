6766dbd3e14deab6c264c1819bfe1c3f48fdc959
depeenderrathore <deepenrathore28@gmail.com>
2015-10-30 01:28:49 +0530

    Project setup

---
+++ urls/app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Urls\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ urls/app/Http/Requests/Request.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Urls\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class Request extends FormRequest
+{
+    //
+}
